865c77a3befe559dc461d9f7149e1696a6bbd0b5
Jahnavi3005 <janumadoju305@gmail.com>
2022-11-30 12:04:00 +0530

    reactjs

---
+++ src/App.js
@@ -0,0 +1,27 @@
+/*import ClassComponent from "./ClassComponent"
+import Component2 from "./Component2"
+
+function App(){
+  return(
+    <section>
+      <img src="https://uploads.sitepoint.com/wp-content/uploads/2017/04/1493235373large_react_apps_A-01.png" alt="react"/>
+      <h1>Good Morning</h1>
+      <p>Hello Everyone</p>
+      <ClassComponent/>
+      <Component2/>    
+    </section>
+  )
+}
+
+export default App;*/
+
+import PropsExample from './PropsExample'
+function App(){
+  return(
+    <div>
+      <h1>This is about Props</h1>
+      <PropsExample name="janu" age="20"/>
+    </div>
+  )
+}
+export default App
---
+++ src/Component2.js
@@ -0,0 +1,11 @@
+import React from "react"
+class Component2 extends React.Component{
+	render(){
+		return(
+			<div>
+				message from Component2
+			</div>
+		)
+	}
+}
+export default Component2 
